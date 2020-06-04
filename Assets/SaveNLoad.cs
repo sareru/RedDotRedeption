b1b51be76bf63fbec2adb569b5f0f4939c08cd06
Stefan Tschunt <smitty2@hotmail.de>
2020-06-04 15:56:18 +0200

    Vorläufige Skripte für Highscore

---
+++ Assets/SaveNLoad.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+public static class SaveSystem
+{
+    public static void SaveScore (Time time)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/highscore/points";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        PlayerData data = new PlayerData(time);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadScore()
+    {
+        string path = Application.persistentDataPath + "/highscore/points";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+        } else
+        {
+            Debug.LogError("Keine Speicherdaten in " + path);
+            return null;
+        }
+
+    }
+}
