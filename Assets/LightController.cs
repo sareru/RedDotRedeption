54bc9da9a91e024996783c30756b4a23372e5590
Dominique Bauer <dominique.s.bauer@gmail.com>
2020-06-06 15:06:10 +0200

    Enemy-Prefab erstellt, kleine Anpassungen bei der Beleuchtung

--- Assets/LightController.cs
+++ Assets/LightController.cs
@@ -6,18 +6,13 @@ public class LightController : MonoBehaviour
     
 {
     Light lt;
+
     // Start is called before the first frame update
     void Start()
     {
 
         lt = GetComponent<Light>();
         lt.intensity = 0;
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 
